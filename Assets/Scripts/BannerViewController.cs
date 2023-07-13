3fffebf9d1836517d77f6ef480117804a8d29dd7
DESKTOP-NBM3K0S\Administrator <870004558@qq.com>
2023-07-13 10:04:01 +0800

    test

---
+++ Assets/Scripts/BannerViewController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+/// <summary>
+/// Demonstrates how to use Google Mobile Ads banner views.
+/// 演示如何使用横幅广告。
+/// </summary>
+[AddComponentMenu("GoogleMobileAds/Samples/BannerViewController")]
+public class BannerViewController : MonoBehaviour
+{
+    /// <summary>
+    /// UI element activated when an ad is ready to show.
+    /// </summary>
+    public GameObject AdLoadedStatus;
+
+    private BannerView _bannerView;
+
+    /// <summary>
+    /// Creates a 320x50 banner at top of the screen.
+    /// </summary>
+    public void CreateBannerView()
+    {
+        //Debug.Log("Creating banner view.");
+        Debug.Log("正在创建横幅视图.");
+
+        // If we already have a banner, destroy the old one.
+        if (_bannerView != null)
+        {
+            DestroyAd();
+        }
+
+        // Create a 320x50 banner at top of the screen.
+        _bannerView = new BannerView(AppOpenAdController._adUnitId,AdSize.Banner, AdPosition.Top);
+
+        // Listen to events the banner may raise.
+        ListenToAdEvents();
+
+        //Debug.Log("Banner view created.");
+        Debug.Log("横幅视图已创建.");
+    }
+
+    /// <summary>
+    /// Creates the banner view and loads a banner ad.
+    /// </summary>
+    public void LoadAd()
+    {
+        // Create an instance of a banner view first.
+        if (_bannerView == null)
+        {
+            CreateBannerView();
+        }
+
+        // Create our request used to load the ad.
+        var adRequest = new AdRequest();
+
+        // Send the request to load the ad.
+        Debug.Log("加载横幅广告.");
+        _bannerView.LoadAd(adRequest);
+    }
+
+    /// <summary>
+    /// Shows the ad.
+    /// </summary>
+    public void ShowAd()
+    {
+        if (_bannerView != null)
+        {
+            Debug.Log("显示横幅广告");
+            _bannerView.Show();
+        }
+    }
+
+    /// <summary>
+    /// Hides the ad.
+    /// </summary>
+    public void HideAd()
+    {
+        if (_bannerView != null)
+        {
+            Debug.Log("隐藏广告。");
+            _bannerView.Hide();
+        }
+    }
+
+    /// <summary>
+    /// Destroys the ad.
+    /// When you are finished with a BannerView, make sure to call
+    /// the Destroy() method before dropping your reference to it.
+    /// </summary>
+    public void DestroyAd()
+    {
+        if (_bannerView != null)
+        {
+            Debug.Log("删除横幅广告");
+            _bannerView.Destroy();
+            _bannerView = null;
+        }
+
+        // Inform the UI that the ad is not ready.
+        AdLoadedStatus?.SetActive(false);
+    }
+
+    /// <summary>
+    /// Logs the ResponseInfo.
+    /// 记录响应信息
+    /// </summary>
+    public void LogResponseInfo()
+    {
+        if (_bannerView != null)
+        {
+            var responseInfo = _bannerView.GetResponseInfo();
+            if (responseInfo != null)
+            {
+                UnityEngine.Debug.Log(responseInfo);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Listen to events the banner may raise.
+    /// 监听横幅可能升起的事件。
+    /// </summary>
+    private void ListenToAdEvents()
+    {
+        // Raised when an ad is loaded into the banner view.
+        _bannerView.OnBannerAdLoaded += () =>
+        {
+            //Debug.Log("Banner view loaded an ad with resposnse : "
+            Debug.Log("横幅视图加载了带有响应的广告: "
+                + _bannerView.GetResponseInfo());
+
+            // Inform the UI that the ad is ready.
+            AdLoadedStatus?.SetActive(true);
+        };
+        // Raised when an ad fails to load into the banner view.
+        _bannerView.OnBannerAdLoadFailed += (LoadAdError error) =>
+        {
+            //Debug.LogError("Banner view failed to load an ad with error : " + error);
+            Debug.LogError("横幅视图加载广告失败，出现错误 : " + error);
+        };
+        // Raised when the ad is estimated to have earned money.
+        _bannerView.OnAdPaid += (AdValue adValue) =>
+        {
+            //Debug.Log(String.Format($"Banner view paid {adValue.Value} {adValue.CurrencyCode}"));
+            Debug.Log(String.Format($"横幅视图已付费 {adValue.Value} {adValue.CurrencyCode}"));
+        };
+        // Raised when an impression is recorded for an ad.
+        _bannerView.OnAdImpressionRecorded += () =>
+        {
+            //Debug.Log("Banner view recorded an impression.");
+            Debug.Log("横幅视图缓存了一个视频");
+        };
+        // Raised when a click is recorded for an ad.
+        _bannerView.OnAdClicked += () =>
+        {
+            Debug.Log("已单击横幅视图.");
+        };
+        // Raised when an ad opened full screen content.
+        _bannerView.OnAdFullScreenContentOpened += () =>
+        {
+            //Debug.Log("Banner view full screen content opened.");
+            Debug.Log("横幅视图全屏内容打开.");
+        };
+        // Raised when the ad closed full screen content.
+        _bannerView.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("横幅视图全屏内容已关闭.");
+            //Debug.Log("Banner view full screen content closed.");
+        };
+    }
+}
---
+++ Assets/Scripts/GoogleMobileAdsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Demonstrates how to use the Google Mobile Ads MobileAds Instance.
+/// 演示如何使用Google Mobile Ads MobileAds实例。
+/// </summary>
+[AddComponentMenu("GoogleMobileAds/Samples/GoogleMobileAdsController")]
+public class GoogleMobileAdsController : MonoBehaviour
+{
+    private static bool _isInitialized;
+
+    /// <summary>
+    /// Initializes the MobileAds SDK
+    /// 初始化MobileAds SDK
+    /// </summary>
+    private void Start()
+    {
+        // Demonstrates how to configure Google Mobile Ads.
+        //演示如何配置谷歌移动广告
+        // Google Mobile Ads needs to be run only once and before loading any ads.
+        //谷歌移动广告只需要在加载任何广告之前运行一次。
+        if (_isInitialized)
+        {
+            return;
+        }
+
+        // On Android, Unity is paused when displaying interstitial or rewarded video.
+        //在Android上，当显示间隙或奖励视频时，Unity会暂停。
+        // This setting makes iOS behave consistently with Android.
+        //此设置使iOS的行为与Android一致。
+        MobileAds.SetiOSAppPauseOnBackground(true);
+
+        // When true all events raised by GoogleMobileAds will be raised
+        //当为true时，GoogleMobileAads引发的所有事件都将被引发
+        // on the Unity main thread. The default value is false.
+        //在Unity主线上。默认值为false。
+        // https://developers.google.com/admob/unity/quick-start#raise_ad_events_on_the_unity_main_thread
+        //在Unity主线程上引发广告事件
+        MobileAds.RaiseAdEventsOnUnityMainThread = true;
+
+        // Set your test devices.
+        // https://developers.google.com/admob/unity/test-ads
+        List<string> deviceIds = new List<string>()
+            {
+                AdRequest.TestDeviceSimulator,
+                // Add your test device IDs (replace with your own device IDs).
+                #if UNITY_IPHONE
+                "ca-app-pub-3940256099942544/2934735716";
+                #elif UNITY_ANDROID
+                "ca-app-pub-1438783342069644~4881382274"
+                #endif
+            };
+
+        // Configure your RequestConfiguration with Child Directed Treatment
+        //使用儿童指导治疗配置您的请求配置以及测试设备Id。
+        // and the Test Device Ids.
+        RequestConfiguration requestConfiguration = new RequestConfiguration
+        {
+            TestDeviceIds = deviceIds
+        };
+        MobileAds.SetRequestConfiguration(requestConfiguration);
+
+        // Initialize the Google Mobile Ads SDK.
+        //Debug.Log("Google Mobile Ads Initializing.");
+        Debug.Log("谷歌移动广告正在初始化。");
+
+        MobileAds.Initialize((InitializationStatus initstatus) =>
+        {
+            if (initstatus == null)
+            {
+                //Debug.LogError("Google Mobile Ads initialization failed.");
+                Debug.LogError("谷歌移动广告初始化失败");
+                return;
+            }
+
+            // If you use mediation, you can check the status of each adapter.
+            //如果使用中介，则可以检查每个适配器的状态。
+            var adapterStatusMap = initstatus.getAdapterStatusMap();
+            if (adapterStatusMap != null)
+            {
+                foreach (var item in adapterStatusMap)
+                {
+                    Debug.Log(string.Format($"Adapter {item.Key} is {item.Value.InitializationState}"));
+                }
+            }
+
+            //Debug.Log("Google Mobile Ads initialization complete.");
+            Debug.Log("谷歌移动广告初始化完成。");
+            _isInitialized = true;
+        });
+    }
+
+    /// <summary>
+    /// Opens the AdInspector.
+    /// </summary>
+    public void OpenAdInspector()
+    {
+        //Debug.Log("Opening ad Inspector.");
+        Debug.Log("打开广告检测");
+        MobileAds.OpenAdInspector((AdInspectorError error) =>
+        {
+            // If the operation failed, an error is returned.
+            //如果操作失败，则返回错误。
+            if (error != null)
+            {
+                //Debug.Log("Ad Inspector failed to open with error: " + error);
+                Debug.Log("广告检查器无法打开，出现错误:" + error);
+                return;
+            }
+
+            //Debug.Log("Ad Inspector opened successfully.");
+            Debug.Log("广告检查器已成功打开。");
+        });
+    }
+}
